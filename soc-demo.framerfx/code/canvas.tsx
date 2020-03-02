1a1dc3966f5a0664d0c1dd51596dc0829070e4df
Nick Weaver <nickweaver@Nicks-MacBook-Pro.local>
2020-03-02 15:35:35 -0700

    added all folders

---
+++ soc-demo.framerfx/code/canvas.tsx
@@ -0,0 +1,4 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Button = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_WPJbMGts2", {}, 120,40);
